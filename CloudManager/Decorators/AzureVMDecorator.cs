88a4014221b6f2eca2911d7f4d6c22816c71c871
marzim <marvin.casagnap@gmail.com>
2020-11-30 18:48:59 +0800

    adding dependencies on azure

--- CloudManager/Decorators/AzureVMDecorator.cs
+++ CloudManager/Decorators/AzureVMDecorator.cs
@@ -3,12 +3,19 @@ namespace CloudManager.Decorators{
     using System;
     using CloudManager.Models;
     using Microsoft.IdentityModel.Clients.ActiveDirectory;
-    
+    using Microsoft.Azure.Management.Compute.Fluent;
+    using Microsoft.Azure.Management.Compute.Fluent.Models;
+    using Microsoft.Azure.Management.Fluent;
+    using Microsoft.Azure.Management.ResourceManager.Fluent;
+    using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
 
     public class AzureVMDecorator : VMWareDecorator
     {
 
         private ICredentials _credential;
+        private CloudManager.Models.IResourceGroup _resourceGroup;
         public AzureVMDecorator(IVMWare _vmware) : base(_vmware)
         {
             _credential = new Credentials();
@@ -17,7 +24,19 @@ namespace CloudManager.Decorators{
             _credential.subscriptionID = "1234";
             _credential.tenantID = "tenant1";
             GetAuthorizationToken();
-            _vmware.adminUser = ""; 
+
+            _vmware.adminUser = "sampluser"; 
+            _vmware.adminPassword = "secretpassword";
+            _vmware.Name = "WinVM";
+            _vmware.vNetName = "az204VNET";            
+            _vmware.vNetAddress = "10.10.0.0/16";
+            _vmware.nicName = "az204NIC";
+            _vmware.subnetAddress = "10.10.0.0/24";
+            _vmware.subnetName = "az204Subnet";
+
+            _resourceGroup = new ResourceGroup();
+            _resourceGroup.groupName = "resourceGroupName1";
+            _resourceGroup.location = "";
             
         }
 
@@ -36,5 +55,7 @@ namespace CloudManager.Decorators{
             }
             _credential.accessToken = result.Result.AccessToken;
         }     
+
+        
     }
 }
